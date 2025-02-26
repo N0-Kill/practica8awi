cda5ff00ebf94ff278d37b2fc025af3388e026b5
N0-Kill <kilitium@gmail.com>
2025-02-26 12:12:04 -0600

    Update conexion

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ from flask_cors import CORS, cross_origin
 
 con = mysql.connector.connect(
     host="185.232.14.52",
-    database="u760464709_16005339_bd",
-    user="u760464709_16005339_usr",
-    password="/iJRzrJBz+P1"
+    database="u760464709_23005263_bd",
+    user="u760464709_23005263_usr",
+    password="8Bw]!4L7*UV"
 )
 
 app = Flask(__name__)
